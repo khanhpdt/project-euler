caf4f1373aaf0312c2e4b6b56aeddb915d075462
khanhpdt <tungkhanh.a2@gmail.com>
2016-10-17 00:00:39 +0700

    - Another solution to problem 1

--- project-euler-java/src/main/java/vn/khanhpdt/playgrounds/projecteuler/MultipleThreeOrFive.java
+++ project-euler-java/src/main/java/vn/khanhpdt/playgrounds/projecteuler/MultipleThreeOrFive.java
@@ -6,7 +6,7 @@ package vn.khanhpdt.playgrounds.projecteuler;
  */
 public class MultipleThreeOrFive {
 
-	public static void main(String[] args) {
+	public static void solutionOne() {
 		final int n = 1_000;
 
 		int result = 0;
@@ -17,6 +17,22 @@ public class MultipleThreeOrFive {
 		}
 
 		System.out.println(result);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(solutionTwo(1000000000));
+	}
+
+	private static long solutionTwo(int n) {
+		return 3 * sumUntil(getMaxMultiple(n, 3)) + 5 * sumUntil(getMaxMultiple(n, 5)) - 15 * sumUntil(getMaxMultiple(n, 15));
+	}
+
+	private static int getMaxMultiple(int n, int k) {
+		return n % k == 0 ? n / k : (n / k) + 1;
+	}
+
+	private static long sumUntil(long maxExclusive) {
+		return ((maxExclusive - 1) * maxExclusive) / 2;
 	}
 
 }
